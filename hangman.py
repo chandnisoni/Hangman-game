8059d04234830d5de408090565be8a5048ef2c95
chandani-soni <chandani.soni@rakuten.com>
2020-01-01 18:41:24 -0500

    first commit

---
+++ hangman.py
@@ -0,0 +1,25 @@
+import string
+
+words = "CHANDANI"
+length = len(words)
+MAX_TRIES = 8
+result = ['_' for i in range(len(words))]
+
+tries=0
+while tries < MAX_TRIES:
+    inp = input("Guess an alphabet: ")
+    count = 0
+    # Did the character that the user guessed exists in the word?
+    if inp in words:
+        pass
+    else:
+        tries += 1
+
+    for x in words:
+        if(x == inp):
+            result[count] = inp
+        count = count+1
+    print(result)
+    print(tries)
+
+print("this is it: ", result)
